f250718029f82284fc52b153cbee6109ac430468
Elshad <safarov.elshad007@gmail.com>
2019-04-18 15:30:41 +0500

    Template and style added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <main className="quote-box">
+            <div className="quote-text">
+                <i className="fa fa-quote-left" aria-hidden="true"></i>
+                <span>Not everything that can be counted counts, and not everything that counts can be counted.</span>
+            </div>
+            <div className="quote-author">- Albert Einstein</div>
+            <div className="buttons">
+                <a href="#"><i className="fa fa-vk" aria-hidden="true"></i></a>
+            </div>
+      </main>
     );
   }
 }
