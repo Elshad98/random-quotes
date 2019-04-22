99a28ab313e2d7f6dcfa64e1c233052432bef5c6
Elshad <safarov.elshad007@gmail.com>
2019-04-22 18:41:08 +0500

    Updates

---
+++ precache-manifest.ee5e6a873c3801501d620e399c1bb33d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcc4046b366da42450f",
+    "url": "/random-quotes/static/js/runtime~main.042febda.js"
+  },
+  {
+    "revision": "a0163ccd279e28556129",
+    "url": "/random-quotes/static/js/main.bc8ca473.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/random-quotes/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "a0163ccd279e28556129",
+    "url": "/random-quotes/static/css/main.6c7ca067.chunk.css"
+  },
+  {
+    "revision": "38801172c78c9bf6ba132240c1599df1",
+    "url": "/random-quotes/index.html"
+  }
+];
